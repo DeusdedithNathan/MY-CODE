b4fae89304f746e2ba447bf7f11da5bf6f6c972a
DeusdedithNathan <deusd2070@gmail.com>
2024-12-28 17:21:43 +0300

    complete Function to generate a random string of fixed length

--- URL_Shortener.cpp
+++ URL_Shortener.cpp
@@ -7,3 +7,10 @@
 // Function to generate a random string of fixed length
 std::string generateShortURL() {
     const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+const size_t length = 6; // Length of the shortened URL
+    std::string shortURL;
+    for (size_t i = 0; i < length; ++i) {
+        shortURL += charset[rand() % (sizeof(charset) - 1)];
+    }
+    return shortURL;
+}
